f239d3c00d7be629d3e8644c63c5a30da552e039
Hasan Atik <hsnatik5050@gmail.com>
2022-04-19 13:52:39 +0300

    learning js

---
+++ döngüler/script.js
@@ -0,0 +1,20 @@
+for (i=0;i<10;i++){
+    console.log("Hello World")
+}
+
+//----------
+
+let sehirler = "'Ankara','istanbul', 'Kayseri', 'Nevsehir' é
+for (i=0; i<sehirler.lenght;i++) {
+    console.log(sehirler[i]+" selam"+i)
+}
+
+//--------
+
+let a=1;
+while(a<10){
+    console.log(a);
+    a=a+1;
+}
+
+//------------
